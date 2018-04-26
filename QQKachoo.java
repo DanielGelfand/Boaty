f1edd4d8ae0f4156009c96b4aded9d426eebc269
DanielGelfand <dgelfand@stuy.edu>
2018-04-25 22:19:53 -0400

    Handle exceptions for offerFirst/Last

--- QQKachoo.java
+++ QQKachoo.java
@@ -25,22 +25,27 @@ public class QQKachoo<Card> implements Deque<Card> {
 
   // default constructor
   public QQKachoo() {
-	  
-  // instantiate the ArrayList
-	deck = new ArrayList<Card>();
+
+    // instantiate the ArrayList
+    deck = new ArrayList<Card>();
 
   }
 
   /** Implementation of the Deque Methods **/
 
   /* ~~~~~ "Add" Methods ~~~~~
-   * Uses the add() methods of ArrayList
-   * Adds element at the beginning or end
-   */
+  * Uses the add() methods of ArrayList
+  * Adds element at the beginning or end
+  */
 
   // Adds element at the beginning of the Deque
   // Returns true if the add is successful and false otherwise
   public boolean offerFirst(Card e) {
+
+    //Handle exception
+    if(e == null){
+      throw new NullPointerException();
+    }
 
     deck.add(0,e);
     return true;
@@ -50,19 +55,23 @@ public class QQKachoo<Card> implements Deque<Card> {
   // Adds element to the end of the Deque
   // Returns true if the add is successful and false otherwise
   public boolean offerLast(Card e) {
-
+    //Handle exception
+    if(e == null){
+      throw new NullPointerException();
+    }
+    
     return deck.add(e);
 
   }
 
   /* ~~~~~ "Remove" Methods ~~~~~
-   * Use the remove() method of ArrayList
-   * Removes either the first or last element
-   */
+  * Use the remove() method of ArrayList
+  * Removes either the first or last element
+  */
 
   // Removes and returns the first element of the ArrayList
   public Card pollFirst() {
-	  
+
     if (deck.size() == 0) {
       return null;
     }
@@ -72,19 +81,19 @@ public class QQKachoo<Card> implements Deque<Card> {
 
   // Removes and returns the last element of the ArrayList
   public Card pollLast() {
-	  
+
     if(deck.size() == 0) {
       return null;
     }
     return deck.remove(deck.size() - 1);
-	
+
 
   }
 
   /* ~~~~~ "Get" Methods ~~~~~
-   * Uses the get() method of ArrayList
-   * retrieve (without removing) either the first or last element
-   */
+  * Uses the get() method of ArrayList
+  * retrieve (without removing) either the first or last element
+  */
 
   public Card peekFirst() {
 
@@ -101,9 +110,9 @@ public class QQKachoo<Card> implements Deque<Card> {
   /* ~~~~~ isEmpty Method ~~~~~ */
 
   public boolean isEmpty() {
-	  
+
     return deck.size() == 0;
-	
+
   }
 
   /** END DEQUE INTERFACE IMPLEMENTATION **/
